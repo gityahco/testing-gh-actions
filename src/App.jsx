37e75a19d4734363c6e0ff9a5efb51f0ccb37abf
Yahya Mardini <120016906+gityahco@users.noreply.github.com>
2023-11-06 09:20:16 +0300

    adding a header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Head/Header";
 function App() {
   return (
     <div className="App">
+      <h1 style={{display: "flex", justifyContent: "center", background: "blue"}}>The Path Finding Visualizer</h1>
       <Header />
       <Index/>
     </div>
